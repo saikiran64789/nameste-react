4cafeaa81ac6378067f2e2a1931c97486f6ac4be
saikiran64789 <soorasaikiran607@gmail.com>
2024-10-27 21:28:17 +0530

    react

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const parent  = React.createElement("div",{id:"Parent"},[
-    React.createElement("div",{id:"child"},[
-     React.createElement("h1",{},"this sai react"),
-     React.createElement("h2",{},"by saikira ")
-   ] ),
-   React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag")
-] ),
-]); 
+import ReactDOM from "react-dom/client";
+/**
+ * 
+ * header
+ * - Logo
+ * - Nav Items
+ * Body
+ *  - Search 
+ *  - RestaurantContainer
+ *   - RestaurantCard 
+ * Footer
+ *  - Copyright
+ *  - links
+ *  - Address
+ *  - Contact
+ * 
+ */
+const Header = () =>{
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img
+        className="logo" 
+        src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"></img>
+         </div>
+         <div className="nav-items">
+          <ul className="items">
+            <li>home</li>
+            <li>About us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+       
+          </ul>
+        </div>
+  </div>
+  );
+};
 
-//jsx
+const RestaurantCard = () =>{
+  return (
+    <div className="res-card">
+      <h3>Meghana Foods</h3>
+     </div>
+  );
+};
 
-console.log(parent);//object 
+const Body = () =>{
+  return(
+    <div className="body">
+    <div className="search">search</div>
+    <div className="res-container">
+      <RestaurantCard/>
+
+    </div>
+  </div>
+  );
+  
+};
+
+const AppLayout  = () =>{
+  return(<div className="app">
+      <Header />
+      <Body/>
+      </div>
+
+      );
+   };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout/>);
+
+
+
